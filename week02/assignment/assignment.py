0bf41ae9a0b3f66f5dd7218a46c18a93ee04ac65
lindaspellman <lindaspellman@live.com>
2023-01-17 12:57:41 -0700

    finished w02 prove asgmt

--- week02/assignment/assignment.py
+++ week02/assignment/assignment.py
@@ -74,15 +74,25 @@ class Request_thread(threading.Thread):
             print('RESPONSE = ', response.status_code)
 
 # TODO Add any functions you need here
-# def add_to_thread_safe_dict(dict, key, val):
-#   lock = threading.Lock() 
-#   with lock:
-#     dict[key] = val 
+# Doesn't work
+'''
+def request_film_item(film_item, master_result_list):
+  film_item_results = []
+  for i in film_item:
+    film_item_threads = Request_thread(i)
+    # Append results to a list and put in a for loop---append is threadsafe
+    film_item_results.append(film_item_threads)
+    film_item_count = len(film_item_results)
+  for i in film_item_results:
+    master_result_list.append(i)
+  return master_result_list
+'''
 
 def main():
     log = Log(show_terminal=True)
     log.start_timer('Starting to retrieve data from the server')
     # make it work under 10 sec
+
     # TODO Retrieve Top API urls
     top = Request_thread(TOP_API_URL)
     top.start()
@@ -103,226 +113,111 @@ def main():
     film6_all = film6_details.response
     # print(f'Film Six Details:')
     # print(film6_all)
-
-    # for i in film6_all:
-    #   # print(i)
-    #   print("---")
-    #   print(i + ': ')
-    #   print(film6_all[i])
     
-    ### Get all the information in Film 6 using Request_thread() and append those threads to the results lists.
-    # full_list = {}
+    ### KEY ADDRESSES OF LISTS OF URL WE NEED TO LOOP THROUGH WITH THREADS
+    characters = film6_all['characters']
+    planets = film6_all['planets']
+    starships = film6_all['starships']
+    vehicles = film6_all['vehicles']
+    species = film6_all['species']
+
+    # DOESN'T WORK
+    # request_film_item(characters, all_results)
+    # request_film_item(planets, all_results)
+    # request_film_item(starships, all_results)
+    # request_film_item(vehicles, all_results)
+    # request_film_item(species, all_results)
+
+    ### MASTER RESULTS LIST
     all_results = []
+
+    ### Loop through all the information in Film 6 using Request_thread(), append those threads to a topic list, and then append the topic list to the all_results master list. Also take note of the length of each topic list b/c you need that later for list slicing.
     # Characters 
     chara_results = []
-    characters = film6_all['characters']
     # print(characters)
     for i in characters:
       # print(i)
       chara_threads = Request_thread(i)
-      # append results to a list and put in a for loop---append is threadsafe
       chara_results.append(chara_threads)
       chara_count = len(chara_results)
     for i in chara_results:
       all_results.append(i)
-      # chara_thread = threading.Thread(target=add_to_thread_safe_dict, args=(full_list, 'characters', i))
-      # full_list['characters'] = i
-      # full_list.append(chara_results)
-    # print(chara_results)
 
     # Planets
     planet_results = []
-    planets = film6_all['planets']
     for i in planets:
       plan_threads = Request_thread(i)
       planet_results.append(plan_threads)
       planet_count = len(planet_results)
     for i in planet_results:
       all_results.append(i)
-      # planet_thread = threading.Thread(target=add_to_thread_safe_dict, args=(full_list, 'planets', i))
-      # full_list['planets'] = i
 
     # Starships
     starship_results = []
-    starships = film6_all['starships']
     for i in starships:
       star_threads = Request_thread(i)
       starship_results.append(star_threads)
       starship_count = len(starship_results)
     for i in starship_results:
       all_results.append(i)
-      # starship_thread = threading.Thread(target=add_to_thread_safe_dict, args=(full_list, 'starships', i))
-      # full_list['starships'] = i
 
     # Vehicles
     vehicle_results = []
-    vehicles = film6_all['vehicles']
     for i in vehicles:
       vehi_threads = Request_thread(i)
       vehicle_results.append(vehi_threads)
       vehicle_count = len(vehicle_results)
     for i in vehicle_results:
       all_results.append(i)
-      # vehicle_thread = threading.Thread(target=add_to_thread_safe_dict, args=(full_list, 'vehicles', i))
-      # full_list['vehicles'] = i
 
     # Species
     species_results = []
-    species = film6_all['species']
     for i in species:
       spec_threads = Request_thread(i)
       species_results.append(spec_threads)
       species_count = len(species_results)
     for i in species_results:
       all_results.append(i)
-      # species_thread = threading.Thread(target=add_to_thread_safe_dict, args=(full_list, 'species', i))
-      # full_list['species'] = i
 
-
-    ### Append the Sorted Lists to a Full List
     ################################
-    ### STARTING THREADS
-    ### Characters
+    ### STARTING ALL THREADS
     for result in all_results:
       result.start()
 
-    # for c in chara_results:
-    #   c.start()
-
-    # ### Planets
-    # for p in planet_results:
-    #   p.start()
-
-    # ### Starships
-    # for s in starship_results:
-    #   s.start()
-
-    # ### Vehicles
-    # for v in vehicle_results:
-    #   v.start()
-
-    # ### Species
-    # for s in species_results:
-    #   s.start()
-
     ################################
-    ### JOINING THREADS
+    ### JOINING THREADS, APPENDING RESULTS TO THE all_names MASTER LIST, SLICING INDICES FROM THE MASTER LIST, AND SORTING THE NEWLY SLICED LISTS
+    ### ALL NAMES
     all_names = []
     for result in all_results:
       result.join()
       all_responses = result.response['name']
-      # all_responses.split(", ")
-      # print(all_responses)
-    # for response in all_responses:
-      # print(response)
       all_names.append(all_responses)
-    print(all_names)
 
+    # print(f"All Names: {all_names}")
+    character_names = all_names[0:chara_count]
+    character_names.sort()
+    # print(f"Character Names: {character_names}")
 
-    ### For each character thread in the chara_results thread list, join it to the main thread. Get the name value from each chara thread and append it to the people list.
-    # people = []
-    # for chara in chara_results:
-    #   chara.join() 
-    #   character_list = chara.response['name']
-    #   people.append(character_list)
-    #   # print(character_list)
+    planet_names_end = chara_count + planet_count
+    planet_names = all_names[chara_count:planet_names_end]
+    planet_names.sort()
+    # print(f"Planet Names: {planet_names}")
 
-    # For each character thread in the chara_results thread list, join it to the main thread. Get the name value from each chara thread and append it to the people list.
-    # planet_list = []
-    # for planet in planet_results:
-    #   planet.join()
-    #   planet_names = planet.response['name']
-    #   planet_list.append(planet_names)
+    starship_names_end = planet_names_end + starship_count
+    starship_names = all_names[planet_names_end:starship_names_end]
+    starship_names.sort()
+    # print(f"Starship Names: {starship_names}")
 
-    # # Starships
-    # starship_list = []
-    # for starship in starship_results:
-    #   starship.join()
-    #   starship_names = starship.response['name']
-    #   starship_list.append(starship_names)
+    vehicle_names_end = starship_names_end + vehicle_count
+    vehicle_names = all_names[starship_names_end:vehicle_names_end]
+    vehicle_names.sort()
+    # print(f"Vehicle Names: {vehicle_names}")
 
-    # # Vehicles
-    # vehicle_list = []
-    # for vehicle in vehicle_results:
-    #   vehicle.join()
-    #   vehicle_names = vehicle.response['name']
-    #   vehicle_list.append(vehicle_names)
+    species_names_end = vehicle_names_end + species_count
+    species_names = all_names[vehicle_names_end:species_names_end]
+    species_names.sort()
+    # print(f"Species Names: {species_names}")
 
-    # # Species
-    # species_list = []
-    # for specie in species_results:
-    #   specie.join()
-    #   species_names = specie.response['name']
-    #   species_list.append(species_names)
-
-    ### SORT THE LISTS
-    people = []
-    for i in all_names:
-      # i.split(",")
-      # print(i)
-      for j in characters:
-        if i == j:
-          people.append(i)
-          # print(i)
-    people.sort()
-    # print(people) # This currently doesn't work.
-
-    planet_output = []
-    for i in all_names:
-      # i.split(",")
-      # print(i)
-      for j in planets:
-        if i == j:
-          planet_output.append(i)
-          # print(i)
-    planet_output.sort()
-
-    starship_output = []
-    for i in all_names:
-      # i.split(",")
-      # print(i)
-      for j in starships:
-        if i == j:
-          starship_output.append(i)
-          # print(i)
-    starship_output.sort()
-
-    vehicle_output = []
-    for i in all_names:
-      # i.split(",")
-      # print(i)
-      for j in vehicles:
-        if i == j:
-          vehicle_output.append(i)
-          # print(i)
-    vehicle_output.sort()
-
-    species_output = []
-    for i in all_names:
-      # i.split(",")
-      # print(i)
-      for j in species:
-        if i == j:
-          species_output.append(i)
-          # print(i)
-    species_output.sort()
-    # print(planet_output) # This currently doesn't work.
-    # starship_list.sort()
-    # vehicle_list.sort()
-    # species_list.sort()
-
-    # for i in film6_all['characters']:
-    #     characters = Request_thread(i)
-    #     characters.start()
-    #     characters.join()
-    #     charas = characters.response
-
-    # for i in charas:
-    #     print(charas)
-
-      
-    # for i in results:
-      ##### create threads
 
     # TODO Display results
     title = film6_all['title']
@@ -330,25 +225,26 @@ def main():
     producer = film6_all['producer']
     release_date = film6_all['release_date']
 
+    log.write('-----------------------------------------')
     log.write('Title   : ' + title)
     log.write('Director: ' + director)
     log.write('Producer: ' + producer)
-    log.write(f'Released: {release_date}')
+    log.write(f'Released: {release_date}' + '\n')
 
     log.write(f'Characters: {chara_count}')
-    log.write(', '.join(people) + '\n')
+    log.write(', '.join(character_names) + '\n')
     
     log.write(f'Planets: {planet_count}')
-    log.write(', '.join(planet_output) + '\n')
+    log.write(', '.join(planet_names) + '\n')
 
     log.write(f'Starships: {starship_count}')
-    log.write(', '.join(starship_output) + '\n')
+    log.write(', '.join(starship_names) + '\n')
 
     log.write(f'Vehicles: {vehicle_count}')
-    log.write(', '.join(vehicle_output) + '\n')
+    log.write(', '.join(vehicle_names) + '\n')
 
     log.write(f'Species: {species_count}')
-    log.write(', '.join(species_output) + '\n')
+    log.write(', '.join(species_names) + '\n')
 
 
     log.stop_timer('Total Time To complete')
